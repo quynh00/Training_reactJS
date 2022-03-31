eaaec9edc8a36243805a102f659b0e5f8b2ca112
Nghiem Thuy Quynh <67702204+quynh00@users.noreply.github.com>
2022-03-31 09:46:40 +0700

    :")

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { } from 'react';
 import Login from './components/login/Login';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes} from 'react-router-dom';
-//import Home from './components/homePage/Home';
-import HomePage from './components/homePage/PrivateRoute';
+import { Navigate, Route, Routes} from 'react-router-dom';
+import Home from './components/homePage/Home';
+// import HomePage from './components/homePage/PrivateRoute';
+import { checkLogin } from './redux/selectors';
+import { useSelector } from 'react-redux';
+
+function PrivateRoute({ children }){
+  const usercheck = useSelector(checkLogin);
+  return usercheck ? children : <Navigate to="/"/>;
+}
+const PrivateRouteLogin = ({ children}) => {
+  const usercheck = useSelector(checkLogin);
+  return !usercheck ? children : <Navigate to="/Home"/>
+};
 
 function App() {
     
     return (
       <>
         <Routes>
-            <Route exact path="/" element={<Login/>}/>
-            <Route path={"/Home/"} element={<HomePage/>}/>            
-            {/* <PrivateRoute path='/Home' component={Home} />  */}
+            <Route exact path="/" element={<PrivateRouteLogin>
+              <Login/>
+            </PrivateRouteLogin>}/>
+            <Route path={"/Home/"} element={<PrivateRoute>
+              <Home/>
+            </PrivateRoute>}/>            
         </Routes>
       </>
     );
